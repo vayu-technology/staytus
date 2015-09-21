3e8694868b1a50def3ff19faa18b5be389a81eb4
Jonathan Clarke <jonathan@vayu.com.au>
2015-09-21 12:58:32 +0545

    Adding schema for postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,31 +13,34 @@
 
 ActiveRecord::Schema.define(version: 20150301145259) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "api_tokens", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "token",      limit: 255
-    t.string   "secret",     limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name"
+    t.string   "token"
+    t.string   "secret"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "authie_sessions", force: :cascade do |t|
-    t.string   "token",              limit: 255
-    t.string   "browser_id",         limit: 255
-    t.integer  "user_id",            limit: 4
-    t.boolean  "active",             limit: 1,     default: true
-    t.text     "data",               limit: 65535
+    t.string   "token"
+    t.string   "browser_id"
+    t.integer  "user_id"
+    t.boolean  "active",             default: true
+    t.text     "data"
     t.datetime "expires_at"
     t.datetime "login_at"
-    t.string   "login_ip",           limit: 255
+    t.string   "login_ip"
     t.datetime "last_activity_at"
-    t.string   "last_activity_ip",   limit: 255
-    t.string   "last_activity_path", limit: 255
-    t.string   "user_agent",         limit: 255
+    t.string   "last_activity_ip"
+    t.string   "last_activity_path"
+    t.string   "user_agent"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "user_type",          limit: 255
-    t.integer  "parent_id",          limit: 4
+    t.string   "user_type"
+    t.integer  "parent_id"
   end
 
   add_index "authie_sessions", ["browser_id"], name: "index_authie_sessions_on_browser_id", using: :btree
@@ -45,15 +48,15 @@ ActiveRecord::Schema.define(version: 20150301145259) do
   add_index "authie_sessions", ["user_id"], name: "index_authie_sessions_on_user_id", using: :btree
 
   create_table "delayed_jobs", force: :cascade do |t|
-    t.integer  "priority",   limit: 4,     default: 0, null: false
-    t.integer  "attempts",   limit: 4,     default: 0, null: false
-    t.text     "handler",    limit: 65535,             null: false
-    t.text     "last_error", limit: 65535
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
     t.datetime "run_at"
     t.datetime "locked_at"
     t.datetime "failed_at"
-    t.string   "locked_by",  limit: 255
-    t.string   "queue",      limit: 255
+    t.string   "locked_by"
+    t.string   "queue"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -61,141 +64,141 @@ ActiveRecord::Schema.define(version: 20150301145259) do
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "email_templates", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "subject",    limit: 255
-    t.text     "content",    limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "name"
+    t.string   "subject"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "history_items", force: :cascade do |t|
-    t.string   "item_type", limit: 255
-    t.integer  "item_id",   limit: 4
+    t.string   "item_type"
+    t.integer  "item_id"
     t.datetime "date"
   end
 
   create_table "issue_service_joins", force: :cascade do |t|
-    t.integer  "issue_id",   limit: 4
-    t.integer  "service_id", limit: 4
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.integer  "issue_id"
+    t.integer  "service_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "issue_updates", force: :cascade do |t|
-    t.integer  "issue_id",          limit: 4
-    t.integer  "user_id",           limit: 4
-    t.integer  "service_status_id", limit: 4
-    t.string   "state",             limit: 255
-    t.text     "text",              limit: 65535
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-    t.string   "identifier",        limit: 255
-    t.boolean  "notify",            limit: 1,     default: false
+    t.integer  "issue_id"
+    t.integer  "user_id"
+    t.integer  "service_status_id"
+    t.string   "state"
+    t.text     "text"
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.string   "identifier"
+    t.boolean  "notify",            default: false
   end
 
   create_table "issues", force: :cascade do |t|
-    t.string   "title",             limit: 255
-    t.string   "state",             limit: 255
-    t.integer  "service_status_id", limit: 4
-    t.boolean  "all_services",      limit: 1,   default: true
-    t.datetime "created_at",                                    null: false
-    t.datetime "updated_at",                                    null: false
-    t.integer  "user_id",           limit: 4
-    t.string   "identifier",        limit: 255
-    t.boolean  "notify",            limit: 1,   default: false
+    t.string   "title"
+    t.string   "state"
+    t.integer  "service_status_id"
+    t.boolean  "all_services",      default: true
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.integer  "user_id"
+    t.string   "identifier"
+    t.boolean  "notify",            default: false
   end
 
   create_table "maintenance_service_joins", force: :cascade do |t|
-    t.integer  "maintenance_id", limit: 4
-    t.integer  "service_id",     limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.integer  "maintenance_id"
+    t.integer  "service_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "maintenance_updates", force: :cascade do |t|
-    t.integer  "maintenance_id", limit: 4
-    t.integer  "user_id",        limit: 4
-    t.text     "text",           limit: 65535
-    t.datetime "created_at",                                   null: false
-    t.datetime "updated_at",                                   null: false
-    t.string   "identifier",     limit: 255
-    t.boolean  "notify",         limit: 1,     default: false
+    t.integer  "maintenance_id"
+    t.integer  "user_id"
+    t.text     "text"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "identifier"
+    t.boolean  "notify",         default: false
   end
 
   create_table "maintenances", force: :cascade do |t|
-    t.string   "title",             limit: 255
-    t.text     "description",       limit: 65535
+    t.string   "title"
+    t.text     "description"
     t.datetime "start_at"
     t.datetime "finish_at"
-    t.integer  "length_in_minutes", limit: 4
-    t.integer  "user_id",           limit: 4
-    t.integer  "service_status_id", limit: 4
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.integer  "length_in_minutes"
+    t.integer  "user_id"
+    t.integer  "service_status_id"
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
     t.datetime "closed_at"
-    t.string   "identifier",        limit: 255
-    t.boolean  "notify",            limit: 1,     default: false
+    t.string   "identifier"
+    t.boolean  "notify",            default: false
   end
 
   create_table "nifty_attachments", force: :cascade do |t|
-    t.integer  "parent_id",   limit: 4
-    t.string   "parent_type", limit: 255
-    t.string   "token",       limit: 255
-    t.string   "digest",      limit: 255
-    t.string   "role",        limit: 255
-    t.string   "file_name",   limit: 255
-    t.string   "file_type",   limit: 255
-    t.binary   "data",        limit: 16777215
+    t.integer  "parent_id"
+    t.string   "parent_type"
+    t.string   "token"
+    t.string   "digest"
+    t.string   "role"
+    t.string   "file_name"
+    t.string   "file_type"
+    t.binary   "data"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "service_statuses", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.string   "permalink",   limit: 255
-    t.string   "color",       limit: 255
-    t.string   "status_type", limit: 255
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.string   "name"
+    t.string   "permalink"
+    t.string   "color"
+    t.string   "status_type"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "services", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.string   "permalink",   limit: 255
-    t.integer  "position",    limit: 4
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.integer  "status_id",   limit: 4
-    t.text     "description", limit: 65535
+    t.string   "name"
+    t.string   "permalink"
+    t.integer  "position"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "status_id"
+    t.text     "description"
   end
 
   create_table "sites", force: :cascade do |t|
-    t.string  "title",               limit: 255
-    t.string  "description",         limit: 255
-    t.string  "domain",              limit: 255
-    t.string  "support_email",       limit: 255
-    t.string  "website_url",         limit: 255
-    t.string  "time_zone",           limit: 255
-    t.boolean "crawling_permitted",  limit: 1,   default: false
-    t.string  "email_from_name",     limit: 255
-    t.string  "email_from_address",  limit: 255
-    t.boolean "allow_subscriptions", limit: 1,   default: true
+    t.string  "title"
+    t.string  "description"
+    t.string  "domain"
+    t.string  "support_email"
+    t.string  "website_url"
+    t.string  "time_zone"
+    t.boolean "crawling_permitted",  default: false
+    t.string  "email_from_name"
+    t.string  "email_from_address"
+    t.boolean "allow_subscriptions", default: true
   end
 
   create_table "subscribers", force: :cascade do |t|
-    t.string   "email_address",      limit: 255
-    t.string   "verification_token", limit: 255
+    t.string   "email_address"
+    t.string   "verification_token"
     t.datetime "verified_at"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email_address",   limit: 255
-    t.string   "name",            limit: 255
-    t.string   "password_digest", limit: 255
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.string   "email_address"
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
