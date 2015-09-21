7284c8596f240f1e50f606e7d98753a739216243
Jonathan Clarke <jonathan@vayu.com.au>
2015-09-21 15:12:20 +0545

    Attempting to fix sendgrid

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -10,10 +10,10 @@
 #   ActionMailer::Base.delivery_method = :sendmail
 # end
 
-Mail.defaults do
-  if ActionMailer::Base.delivery_method == :smtp
-    delivery_method :smtp, ActionMailer::Base.smtp_settings
-  else
-    delivery_method ActionMailer::Base.delivery_method
-  end
-end
+# Mail.defaults do
+#   if ActionMailer::Base.delivery_method == :smtp
+#     delivery_method :smtp, ActionMailer::Base.smtp_settings
+#   else
+#     delivery_method ActionMailer::Base.delivery_method
+#   end
+# end
